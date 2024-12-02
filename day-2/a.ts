54a7117b1ed85499a98f1dd0eee0b11f9a876d55
Thomas den Boer <thomas.den.boer@rosemanlabs.com>
2024-12-02 19:27:24 +0100

    day2

--- day-2/a.ts
+++ day-2/a.ts
@@ -1,9 +1,35 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
+
+type Level = number;
+type Report = Level[];
 
 /** provide your solution as the return of this function */
 export async function day2a(data: string[]) {
-  console.log(data);
-  return 0;
+  const reports = parseDay2(data);
+  const safeReports = reports.map((report) =>
+    isSafe(report))
+  ;
+  return safeReports.filter((val) => val).length;
+}
+
+function isSafe(report: Report): boolean {
+  const sorted = report.toSorted((a, b) => a - b)
+  if (report.toString() !== sorted.toString() && report.toReversed().toString() !== sorted.toString()) {
+    return false;
+  }
+  for (let i = 1; i < report.length; i++) {
+    const step = Math.abs(report[i] - report[i - 1]);
+    if (step < 1 || step > 3) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function parseDay2(data: string[]): Report[] {
+  return data
+    .filter((x) => x.trim() !== "")
+    .map((report) => report.split(" ").map((val) => parseInt(val)));
 }
 
 await runSolution(day2a);
--- day-2/b.ts
+++ day-2/b.ts
@@ -1,9 +1,45 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
+
+type Level = number;
+type Report = Level[];
 
 /** provide your solution as the return of this function */
-export async function day2b(data: string[]) {
-  console.log(data);
-  return 0;
+export async function day2a(data: string[]) {
+  const reports = parseDay2(data);
+  const errorCorrectedReports = reports.map((report) => isSafe(report))
+    .map((safe, reportIdx) => {
+      if (!safe) {
+        const unsafeReport = reports[reportIdx];
+        console.log("unsafe report", reports[reportIdx])
+        for (let i = 0; i < unsafeReport.length; i++) {
+          if (isSafe(unsafeReport.toSpliced(i, 1))) {
+            return true;
+          }}
+        
+      }
+      return safe;
+    })
+  return errorCorrectedReports.filter((val) => val).length;
 }
 
-await runSolution(day2b);
+function isSafe(report: Report): boolean {
+  const sorted = report.toSorted((a, b) => a - b);
+  if (report.toString() !== sorted.toString() && report.toReversed().toString() !== sorted.toString()) {
+    return false;
+  }
+  for(let i = 1; i < report.length; i++) {
+    const step = Math.abs(report[i] - report[i-1])
+    if (step < 1 || step > 3) {
+      return false;
+    }
+  };
+  return true;
+}
+
+function parseDay2(data: string[]): Report[] {
+  return data
+    .filter((x) => x.trim() !== "")
+    .map((report) => report.split(" ").map((val) => parseInt(val)));
+}
+
+await runSolution(day2a);
