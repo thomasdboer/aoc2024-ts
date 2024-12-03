cb3753db9877f37d21e308d5504e31c65e5edf35
Thomas den Boer <thomas.den.boer@rosemanlabs.com>
2024-12-03 11:52:32 +0100

    further cleanup

--- day-2/a.ts
+++ day-2/a.ts
@@ -1,32 +1,35 @@
 import { runSolution } from "../utils.ts";
 
-type Level = number;
-type Report = Level[];
+export type Level = number;
+export type Report = Level[];
 
 /** provide your solution as the return of this function */
 export async function day2a(data: string[]) {
   const reports = parseDay2(data);
-  const safeReports = reports.map((report) =>
-    isSafe(report))
-  ;
-  return safeReports.filter((val) => val).length;
+  return reports.map((report) => isSafe(report)).filter((val) => val).length;
 }
 
-function isSafe(report: Report): boolean {
-  const sorted = report.toSorted((a, b) => a - b)
-  if (report.toString() !== sorted.toString() && report.toReversed().toString() !== sorted.toString()) {
-    return false;
-  }
-  for (let i = 1; i < report.length; i++) {
-    const step = Math.abs(report[i] - report[i - 1]);
+export function isSafe(report: Report): boolean {
+  const sorted = report.toSorted((a, b) => a - b);
+  return !(isNotIncreasingOrDecreasing(report, sorted) || hasIllegalStepSize(report))
+}
+
+export function isNotIncreasingOrDecreasing(report: Report, sorted: Report) {
+  return (report.toString() !== sorted.toString() && 
+      report.toReversed().toString() !== sorted.toString())
+}
+
+export function hasIllegalStepSize(report: Report) {
+  for(let i = 1; i < report.length; i++) {
+    const step = Math.abs(report[i] - report[i-1])
     if (step < 1 || step > 3) {
-      return false;
+      return true;
     }
-  }
-  return true;
+  };
+  return false;
 }
 
-function parseDay2(data: string[]): Report[] {
+export function parseDay2(data: string[]): Report[] {
   return data
     .filter((x) => x.trim() !== "")
     .map((report) => report.split(" ").map((val) => parseInt(val)));
--- day-2/b.ts
+++ day-2/b.ts
@@ -1,45 +1,24 @@
 import { runSolution } from "../utils.ts";
-
-type Level = number;
-type Report = Level[];
+import { isSafe, parseDay2, Report } from "./a.ts";
 
 /** provide your solution as the return of this function */
 export async function day2a(data: string[]) {
   const reports = parseDay2(data);
-  const errorCorrectedReports = reports.map((report) => isSafe(report))
-    .map((safe, reportIdx) => {
-      if (!safe) {
-        const unsafeReport = reports[reportIdx];
-        console.log("unsafe report", reports[reportIdx])
-        for (let i = 0; i < unsafeReport.length; i++) {
-          if (isSafe(unsafeReport.toSpliced(i, 1))) {
-            return true;
-          }}
-        
-      }
-      return safe;
-    })
-  return errorCorrectedReports.filter((val) => val).length;
+  return reports
+    .map((report) => isSafe(report))
+    .map((safe, idx) => safe ? safe : isAnyWindowSafe(reports[idx]))
+    .filter((val) => val)
+    .length;
 }
 
-function isSafe(report: Report): boolean {
-  const sorted = report.toSorted((a, b) => a - b);
-  if (report.toString() !== sorted.toString() && report.toReversed().toString() !== sorted.toString()) {
-    return false;
-  }
-  for(let i = 1; i < report.length; i++) {
-    const step = Math.abs(report[i] - report[i-1])
-    if (step < 1 || step > 3) {
-      return false;
+function isAnyWindowSafe(report: Report) {
+  for (let i = 0; i < report.length; i++) {
+    const window = report.toSpliced(i, 1)
+    if (isSafe(window)) {
+      return true;
     }
-  };
-  return true;
-}
-
-function parseDay2(data: string[]): Report[] {
-  return data
-    .filter((x) => x.trim() !== "")
-    .map((report) => report.split(" ").map((val) => parseInt(val)));
+  }
+  return false;
 }
 
 await runSolution(day2a);
