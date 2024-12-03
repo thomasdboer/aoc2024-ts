91e61a1ee1811b0c8a7247889095ae7f28ff5275
Thomas den Boer <thomas.den.boer@rosemanlabs.com>
2024-12-03 12:53:13 +0100

    d3a

--- day-3/a.ts
+++ day-3/a.ts
@@ -1,9 +1,25 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
-/** provide your solution as the return of this function */
+const MUL_REGEXP = /(?:mul\(\d{1,3},\d{1,3}\))/g;
+const DIGITS_REGEXP = /\d{1,3}/g; /** provide your solution as the return of this function */
+
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+  const instructions = parseDay3(data);
+  return instructions.reduce((total, curr) => total + curr[0] * curr[1], 0);
+}
+
+function parseDay3(data: string[]): number[][] {
+  return data
+    .filter((x) => x.trim() !== "")
+    .map((element) =>
+      element.match(MUL_REGEXP).map((val) =>
+        val
+          .split(",")
+          .flatMap((part) => part.match(DIGITS_REGEXP))
+          .flatMap((digit) => parseInt(digit)),
+      ),
+    )
+    .flat();
 }
 
 await runSolution(day3a);
