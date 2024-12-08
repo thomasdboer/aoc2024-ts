8c300901aac2562628c1e07c586ad8f3ca1d8f17
Thomas den Boer <thomas.den.boer@rosemanlabs.com>
2024-12-08 12:33:23 +0100

    i hate regex

--- day-3/b.ts
+++ day-3/b.ts
@@ -1,7 +1,7 @@
 import { runSolution } from "../utils.ts";
 import { DIGITS_REGEXP, MUL_REGEXP } from "./a.ts";
 
-const DONT_REGEXP = /(don't\(\).*?do\(\))+/g;
+const DONT_REGEXP = /(don't\(\).*?(do\(\)|$))+/g;
 
 /** provide your solution as the return of this function */
 export async function day3b(data: string[]) {
@@ -11,14 +11,14 @@ export async function day3b(data: string[]) {
 function parse(data: string[]): number[][] {
   return data
     .filter((x) => x.trim() !== "")
-    .map((line) => line.replace(DONT_REGEXP, ""))
-    .flatMap((line) =>
-      line.match(MUL_REGEXP).map((instruction) =>
-        instruction
-          .split(",")
-          .flatMap((part) => part.match(DIGITS_REGEXP))
-          .flatMap((digit) => parseInt(digit)),
-      ),
+    .join("")
+    .replace(DONT_REGEXP, "")
+    .match(MUL_REGEXP)
+    .map((mul) =>
+      mul
+        .split(",")
+        .flatMap((part) => part.match(DIGITS_REGEXP))
+        .flatMap((digit) => parseInt(digit)),
     );
 }
 
